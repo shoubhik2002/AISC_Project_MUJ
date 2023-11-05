d0c4457c3b305677aa23589e44642079de78338f
shoubhik2002 <96007672+shoubhik2002@users.noreply.github.com>
2023-11-05 22:28:52 +0530

    Update inception_resnet_v2.py

--- inception_resnet_v2.py
+++ inception_resnet_v2.py
@@ -1,28 +1,3 @@
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-# The content is derived from https://github.com/tensorflow/models/blob/master/slim/nets/inception_resnet_v2.py
-# ==============================================================================
-
-"""Contains the definition of the Inception Resnet V2 architecture.
-
-As described in http://arxiv.org/abs/1602.07261.
-
-  Inception-v4, Inception-ResNet and the Impact of Residual Connections
-    on Learning
-  Christian Szegedy, Sergey Ioffe, Vincent Vanhoucke, Alex Alemi
-"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -96,21 +71,7 @@ def block8(net, scale = 1.0, activation_fn = tf.nn.relu, scope = None, reuse = N
 def inception_resnet_v2(inputs, is_training = True,
                             reuse = None,
                             scope = 'InceptionResnetV2'):
-    """Creates the Inception Resnet V2 model.
 
-  Args:
-    inputs: a 4-D tensor of size [batch_size, height, width, 3].
-    num_classes: number of predicted classes.
-    is_training: whether is training or not.
-    dropout_keep_prob: float, the fraction to keep before final layer.
-    reuse: whether or not the network and its variables should be reused. To be
-      able to reuse 'scope' must be given.
-    scope: Optional variable_scope.
-
-  Returns:
-    logits: the logits outputs of the model.
-    end_points: the set of end_points from the inception model.
-  """
     end_points = { }
 
     with tf.variable_scope(scope, 'InceptionResnetV2', [inputs], reuse = reuse):
@@ -211,17 +172,7 @@ inception_resnet_v2.default_image_size = 299
 def inception_resnet_v2_arg_scope(weight_decay = 0.00004,
                                   batch_norm_decay = 0.9997,
                                   batch_norm_epsilon = 0.001):
-    """Yields the scope with the default parameters for inception_resnet_v2.
 
-  Args:
-    weight_decay: the weight decay for weights variables.
-    batch_norm_decay: decay for the moving average of batch_norm momentums.
-    batch_norm_epsilon: small float added to variance to avoid dividing by zero.
-
-  Returns:
-    a arg_scope with the parameters needed for inception_resnet_v2.
-  """
-    # Set weight_decay for weights in conv2d and fully_connected layers.
 
     with slim.arg_scope([slim.conv2d, slim.fully_connected],
                         weights_regularizer = slim.l2_regularizer(weight_decay),
